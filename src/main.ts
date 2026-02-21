baf8202e04432217fc28471f3c25a416885c13cc
mamaroma <mrvelialman@gmail.com>
2026-02-21 08:30:48 +0300

    slkfjojfkvp

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Static assets (CSS/JS/images)
-  app.useStaticAssets(join(__dirname, '..', 'public'));
+    app.useStaticAssets(join(process.cwd(), 'public'), {
+        index: false,
+    });
 
-  // Views (EJS templates)
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('ejs');
+    app.setBaseViewsDir(join(process.cwd(), 'views'));
+    app.setViewEngine('ejs');
 
-  const configService = app.get(ConfigService);
-  const port = Number(configService.get('PORT') ?? 3000);
-
-  await app.listen(port);
+    const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+    await app.listen(port, '0.0.0.0');
 }
-
 bootstrap();
